d466cd5e45548eede2c8efbb87936c45913ddba5
João Vitor <joaovsilvateixeira@gmail.com>
2023-05-21 00:09:49 -0300

    Capsula do Tempo

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Todos os direitos reservados a{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://www.linkedin.com/in/joaovitorsilva8101/"
+      >
+        João Vitor Silva Teixeira
+      </a>
+    </div>
+  )
+}
